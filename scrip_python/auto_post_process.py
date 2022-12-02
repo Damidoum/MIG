e09175d6c3640cd261d4af3abe320e523f3d5647
Damien Rouchouse <damien.rouchouse@etu.minesparis.psl.eu>
2022-12-02 20:04:49 +0100

    ajout d'une fonction pour récupérer le post process dans le script auto_post_process.py

--- scrip_python/auto_post_process.py
+++ scrip_python/auto_post_process.py
@@ -5,6 +5,7 @@ import sys
 
 
 def prost_process_file(classe):
+    """Mise en place des fichiers de post process et de maillage"""
     for k in range(10):
         shutil.copyfile(
             "/home/user/store/data/Preparation INP/create_group.inp",
@@ -27,6 +28,7 @@ def prost_process_file(classe):
 
 
 def run_mesh(classe):
+    """Création du maillage"""
     for k in range(10):
         os.chdir(
             "/home/user/store/data/calcul_zoom_"
@@ -39,6 +41,7 @@ def run_mesh(classe):
 
 
 def run_post(classe):
+    """Lancement des posts process"""
     for k in range(10):
         os.chdir(
             "/home/user/store/data/calcul_zoom_"
@@ -50,10 +53,31 @@ def run_post(classe):
         subprocess.call("xterm -e Zrun extract_on_group.inp", shell=True)
 
 
+def get_post_process(classe):
+    """Récupération des fichiers du post process"""
+    if not os.path.exists("/home/user/store/data/Krigeage/Classe" + str(classe)):
+        os.mkdir("/home/user/store/data/Krigeage/Classe" + str(classe))
+    for k in range(10):
+        shutil.copyfile(
+            "/home/user/store/data/calcul_zoom_"
+            + str(classe)
+            + "/calcul_zoom_"
+            + str(classe)
+            + str(k)
+            + "/extract_on_group.post",
+            "/home/user/store/data/Krigeage/Classe"
+            + str(classe)
+            + "/extract_on_group"
+            + str(k)
+            + ".post",
+        )
+
+
 def main(classe):
     prost_process_file(classe)
     run_mesh(classe)
     run_post(classe)
+    get_post_process(classe)
 
 
 if __name__ == "__main__":
