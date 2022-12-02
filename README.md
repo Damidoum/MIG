fef21639cc76f15c5eacffd097b0152b4fefc533
Damien Rouchouse <damien.rouchouse@etu.minesparis.psl.eu>
2022-12-02 21:15:13 +0100

    Ajout des scripts de krigeage

--- README.md
+++ README.md
@@ -7,3 +7,20 @@ Quelques liens :
 Test-Classification -> familirisation avec la notion de classification des séries temporelles. On teste les fonctions sur des données covid. 
 
 TraitementEpilon -> importation des données du tenseur de déformation, traitement de ces données et création de classes appropriées. 
+
+kriging_final -> nécéssite les données au format krig_class{i}.csv, fait le krigeage, sauvegarde le metamodèle dans une study openturns et les courbes d'approximation du modèle en png
+
+pour l'utilisation du fichier xml:
+import openturns as ot
+
+study = ot.Study()
+
+study.setStorageManager(ot.XMLStorageManager(f'metamodel_par_classe.xml'))
+
+i = 5 #numéro de la classe
+
+study.load() #charge le fichier
+
+metamodel = ot.Function() #crée un metamodèle vide
+
+study.fillObject(f'metamodel_class{i}', metamodel) #metamodel est le modèle en question, il prend un tuple (espilon300, epsilon600) et renvoie la valeur de sigma
---
+++ scrip_python/krigeage/apply_krigeage.py
@@ -0,0 +1,35 @@
+import openturns as ot
+import pandas as pd 
+import numpy as np 
+
+study = ot.Study()
+study.setStorageManager(ot.XMLStorageManager(f'metamodel_par_classe.xml'))
+study.load()
+lst = [ot.Function() for _ in range(10)]
+for i in range(10):
+    if i != 2: 
+        study.fillObject(f'metamodel_class{i}', lst[i]) 
+
+df = pd.read_csv('for_krigeage.csv', index_col = 'node')
+dico = {}
+for x in df.index : 
+    dico[x] = int(x.split('_')[1])
+df = df.rename(dico)
+df = df.rename({'3630.0' : '600.0'}, axis = 1)
+df2 = df.groupby('classe')
+
+res = []
+for k in range(10): 
+    if k!=2: 
+        df3 = df2.get_group(k)
+        l = [lst[k]((df3.iloc[i,0],df3.iloc[i,1]))[0] for i in range(len(df3))]
+        df3['sig'] = l
+        res.append(pd.DataFrame(df3['sig']))
+    else : 
+        df3 = df2.get_group(k)
+        l = [0 for _ in range(len(df3))]
+        df3['sig'] = l
+        res.append(pd.DataFrame(df3['sig']))
+df_res = pd.concat(res)
+df_res.to_csv('sig.csv')
+
